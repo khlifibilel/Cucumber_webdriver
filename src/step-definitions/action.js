0dedd38df8d5549e8e3ef8505e5ffe7db99367eb
khlifibilel <bilel.khlifi.1@esprit.tn>
2022-11-06 23:26:05 +0100

    sans page object

--- src/step-definitions/action.js
+++ src/step-definitions/action.js
@@ -8,7 +8,7 @@ When (/^Je clique sur le lien "(.*)"$/, async (lien) => {
     await $(lien).click()             
 });
 
-/// hello 
+// action
 
 
 When (/^Je clique sur le lien aide$/, async () => {       
